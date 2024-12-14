b421beba4e87d9cb11e4f3e2275f4fe52d5e2aff
bintangmuh <bintangmhd@gmail.com>
2024-12-14 10:57:26 +0700

    aelf code - select

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,13 +5,30 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+const StyleWrapper = styled.select`
+  border: none;
+  background-color: ${COLORS.transparentGray15};
+  padding: 12px 16px;
+  border-radius: 8px;
+
+  color: ${COLORS.gray700};
+
+  &:focus {
+    outline: 2px solid ${COLORS.primary};
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <StyleWrapper value={value} onChange={onChange}>
       {children}
-    </select>
+    </StyleWrapper>
   );
 };
 
